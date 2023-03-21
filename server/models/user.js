d77cbdf86494740be7688106d062c42ebe7c3339
Abdallah Abu khdair <71616389+abdullahab1968@users.noreply.github.com>
2023-03-21 15:08:19 +0200

    model schema editing

--- server/models/user.js
+++ server/models/user.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost/onlineShopping")
+  .connect("mongodb+srv://abdallahabukhdair:3A8o3Xa9isjMTmzd@cluster0.uvh8ikk.mongodb.net/?retryWrites=true&w=majority")
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.log("cant Connected to MongoDB", error));
+
 
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
   budget: Number,
+  orders: [{type: mongoose.Schema.Types.ObjectId, ref: "Order"}],
 });
 
 const User = mongoose.model("User", userSchema);
