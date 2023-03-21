fa5de6d837ee3e590958f3633d7aada394bc8819
malak <malak.abogdaya@gmail.com>
2023-03-21 19:48:23 +0200

    get undelivered orders

--- dist/orders-traking/render.js
+++ dist/orders-traking/render.js
@@ -1,24 +1,25 @@
 const Render = function () {
-  const orders = [
-    {
-      id: 0,
-      appIcon:
-        "https://cdn.vox-cdn.com/thumbor/b3Hilihn-dxB4aJBvIF2_KrZpoY=/0x0:1200x630/1400x1400/filters:focal(600x315:601x316)/cdn.vox-cdn.com/uploads/chorus_asset/file/22259693/download.png",
-      days: 12,
-      dayesPassed: 5,
-    },
-    {
-      id: 1,
-      appIcon:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp9AJ4R7eyzC3EJsuZqqW90eK2tPw62l__n2vV_Yxh6g&s",
-      days: 7,
-      dayesPassed: 3,
-    },
-  ];
+  const orders = [];
+  // const orders = [
+  //   {
+  //     id: 0,
+  //     appIcon:
+  //       "https://cdn.vox-cdn.com/thumbor/b3Hilihn-dxB4aJBvIF2_KrZpoY=/0x0:1200x630/1400x1400/filters:focal(600x315:601x316)/cdn.vox-cdn.com/uploads/chorus_asset/file/22259693/download.png",
+  //     days: 12,
+  //     dayesPassed: 5,
+  //   },
+  //   {
+  //     id: 1,
+  //     appIcon:
+  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp9AJ4R7eyzC3EJsuZqqW90eK2tPw62l__n2vV_Yxh6g&s",
+  //     days: 7,
+  //     dayesPassed: 3,
+  //   },
+  // ];
 
-  //   const getUnDeliveredOrdersFromDB = function () {
-  //     // give each order feild days and id
-  //   };
+    const getUnDeliveredOrdersFromDB = function () {
+        
+    };
 
   const renderUnDeliveredOrders = function () {
     const orderSource = $("#order").html();
