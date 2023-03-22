7f6ddd05f8190bd2a1c5725dc52005afc31309d1
malak <malak.abogdaya@gmail.com>
2023-03-22 12:05:06 +0200

    update passed days

--- dist/orders-traking/main.js
+++ dist/orders-traking/main.js
@@ -1,3 +1,5 @@
+const schedule = require("node-schedule");
+
 const render = Render();
 render.renderUnDeliveredOrders();
 
@@ -9,3 +11,14 @@ function addOrder() {
   window.location.href = "../order/order_form.html";
 }
 
+$("#orders-container").on("click", ".delivered", removeElement);
+function removeElement() {
+  const id = $(this).closest(".order").find(".bar").data().id;
+  $.ajax({ method: "PUT", url: `http://localhost:3000/order/update?id=${id}` });
+  $(this).closest(".order").empty();
+}
+
+// run the function at midnight
+const startNewDay = schedule.scheduleJob("01 00 00 * * *", function () {
+  render.renderUnDeliveredOrders();
+});
